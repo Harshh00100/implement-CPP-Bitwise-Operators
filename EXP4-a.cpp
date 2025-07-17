d36f5ee1a0c8de9de74eb1130ca69ce008f7cde1
Harshh00100 <kunarharsh76@gmail.com>
2025-07-17 19:52:02 +0530

    Add files via upload

---
+++ EXP4-a.cpp
@@ -0,0 +1,28 @@
+// Harsh Kumar
+// 24070123146  
+// B3
+#include <iostream>
+using namespace std;
+int main() { 
+    int a=10, b=14; // Initialize two integer variables   1010 and 1110 in binary
+    int A = a&b; // Bitwise AND operation
+    int B = a|b; // Bitwise OR operation    
+    int C = a^b; // Bitwise XOR operation
+    int nA = ~a; // Bitwise NOT operation
+    int nB = ~b; // Bitwise NOT operation
+    int sA = a<<1; // Left shift operation
+    int sB = b<<1; 
+    int rA = a>>1; // Right shift operation 
+    int rB = b>>1; 
+    // Displaying the results of bitwise operations
+    cout << "Bitwise AND: " << A << "\n";
+    cout << "Bitwise OR: " << B << "\n";
+    cout << "Bitwise XOR: " << C << "\n";   
+    cout << "Bitwise NOT of a: " << nA << "\n";
+    cout << "Bitwise NOT of b: " << nB << "\n";
+    cout << "Left shift of a: " << sA << "\n";
+    cout << "Left shift of b: " << sB << "\n";
+    cout << "Right shift of a: " << rA << "\n";
+    cout << "Right shift of b: " << rB << "\n";
+    return 0; 
+}
---
+++ EXP4-b.cpp
@@ -0,0 +1,19 @@
+// Harsh Kumar
+// 24070123146  
+// B3
+#include <iostream>
+using namespace std;
+int main() {
+    int a=1200;
+    int bit_set ,bit_reset, bit_setting,bit_resetting;
+    // Setting the 3rd bit (counting from 0)
+    cout << "enter the bit which need to be set: ";
+    cin >> bit_set;
+    cout << "enter the bit which need to be reset: ";
+    cin >> bit_reset; 
+    bit_setting= a| (1 << bit_set);
+    bit_resetting = a & ~(1 << bit_reset);
+    cout << "After setting bit " << bit_set << ": " << bit_setting << "\n";
+    cout << "After resetting bit " << bit_reset << ": " << bit_resetting << "\n";
+    return 0;
+}
